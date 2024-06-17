2c9a5bbc86f291a107b19a5a530d9010f3e248d5
keyush-parwal <keyushparwal1@gmail.com>
2024-06-17 11:56:14 +0530

    Create Shop Cart Functionality

--- frontend/src/Context/StoreContext.js
+++ frontend/src/Context/StoreContext.js
@@ -10,17 +10,22 @@ const StoreContextProvider=(props)=>{
     const [token,setToken]=useState('');
     const [food_list,setFoodList]=useState([]);
 
-    const addToCart=(itemId)=>{
+    const addToCart=async (itemId)=>{
         if(!cartItems[itemId]){
             SetcartItems((prev)=>({...prev,[itemId]:1}));
         }
         else{
             SetcartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
         }
+
+        if(token){
+            await axios.post(url+'/api/cart/add',{itemId},{headers:{token}})
+        }
     }
 
-    const removeFromCart=(itemId)=>{
+    const removeFromCart= async (itemId)=>{
         SetcartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
+        await axios.post(url+'/api/cart/remove',{itemId},{headers:{token}})
     }
 
     const getTotalCartAmount=()=>{
@@ -42,6 +47,14 @@ const StoreContextProvider=(props)=>{
         setFoodList(response.data.data);
     }
 
+
+
+    // if we refresh the page then also we want to keep the cart items in the cart
+    const loadCartData = async (token) => {
+        const response=await axios.post(url+'/api/cart/get',{},{headers:{token}});
+        SetcartItems(response.data.cartData);
+    }
+
     // If we relaod the page so we don't get logout , we want to keep the token in the local storage
     useEffect(()=>{
 
@@ -49,6 +62,7 @@ const StoreContextProvider=(props)=>{
             await fetchFoodList();
             if(localStorage.getItem('token')){
                 setToken(localStorage.getItem('token'));
+                await loadCartData(localStorage.getItem('token'));
             }
         }
 
